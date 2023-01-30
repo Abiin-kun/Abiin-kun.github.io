9c07a2e264d53b686c59a425cdc6d3ea6072c102
abiinkunlovescats <pabhinn771@outlook.com>
2023-01-30 19:54:53 +0530

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+  import Home from "./lib/Home.svelte";
+  import {currentPage} from "./store.js";
+  import WIP from "./lib/WIP.svelte";
+
+</script>
+
+<main>
+    <div class="navbar">
+      <p class="text" >Abhinn Pandey's Personal Space</p>
+      <div class="items">
+        <button on:click={() => $currentPage = "Home"}>Home</button>
+        <button on:click={() => $currentPage = "WIP"}>Blogs</button>
+        <button on:click={() => $currentPage = "WIP"}>Projects</button>
+        <button><a href="https://www.github.com/abiin-kun/">Github</a></button>
+
+      </div>
+
+    </div>
+
+
+
+
+    {#if $currentPage === "Home"}
+      <div class="container">
+      <Home />
+      </div>
+      {:else if $currentPage === "WIP"}
+      <div class="container" style="background: black; height: 100%">
+      <h1 style="text-align: center" class="text">This Page is in Development but dont worry this page will come blazingly 🔥 soon</h1>
+      </div>
+    {/if}
+
+
+
+
+</main>
+
+<style>
+  .navbar {
+    position: sticky;
+    width: 100%;
+    height: 50px;
+    background-color: black;
+    display: flex;
+    align-items: center;
+    margin-left: auto;
+    padding-left: 10px;
+  }
+
+  /*.container {*/
+  /*  display: flex;*/
+  /*  justify-content: center;*/
+  /*  align-items: center;*/
+  /*  height: calc(100vh - 100px);*/
+  /*}*/
+  .items {
+    margin: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+ .text{
+  color: white;
+}
+</style>
---
+++ src/lib/WIP.svelte
@@ -0,0 +1,1 @@
+<h1 style="text-align: center">This Page is in Development but dont worry this page will come blazingly 🔥 soon</h1>
