3b769c97486703c3f24f217932af2b6d6b51fb18
Abhinn Pandey <123942795+Abiin-kun@users.noreply.github.com>
2024-09-09 16:39:35 +0000

    FIX ERROR

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,12 @@ import sitemap from '@astrojs/sitemap';
 
 import svelte from '@astrojs/svelte';
 
-import node from '@astrojs/node';
 
 import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
+
 export default defineConfig({
-  site: 'https://example.com',
+
   integrations: [mdx(), sitemap(), svelte()],
   output: 'server',
 
