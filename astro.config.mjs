e3f5d1357b7b44fe89f8aedffe3eb843d11de892
Abhinn Pandey <123942795+Abiin-kun@users.noreply.github.com>
2024-09-10 14:54:45 +0000

    Fixed Typescript

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import vercel from '@astrojs/vercel/serverless';
 
 
 export default defineConfig({
-
+  site: "https://abhinn.vercel.app",
   integrations: [mdx(), sitemap()],
   output: 'server',
 
